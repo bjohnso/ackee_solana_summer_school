869ed7c9f47aea37bb347a8fb1de26b3dcd9a8ee
bjohnso <bjohnso@student.wethinkcode.co.za>
2022-08-08 00:09:23 +0200

    initali commit

--- programs/auction/src/lib.rs
+++ programs/auction/src/lib.rs
@@ -2,51 +2,259 @@ use anchor_lang::{
     prelude::*,
     solana_program::clock::UnixTimestamp,
 };
+use anchor_lang::solana_program::clock;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AcLrVrFyxpguUKDLLwxevDviGqzimyU4ynXyVZggc8oH");
 
 #[program]
 pub mod auction {
+    use anchor_lang::solana_program::native_token::sol_to_lamports;
+    use anchor_lang::solana_program::program::{invoke, invoke_signed};
+    use anchor_lang::solana_program::{clock, system_instruction};
+    use anchor_lang::solana_program::system_instruction::SystemInstruction::Transfer;
     use super::*;
     /// Creates and initialize a new state of our program
-    pub fn initialize(ctx: Context<Initialize>, auction_duration: UnixTimestamp, /* optional parameters */) -> Result<()> {
-        // ...
+    pub fn initialize(ctx: Context<Initialize>, auction_duration: u64, /* optional parameters */) -> Result<()> {
+
+        require!(auction_duration > now_ts().unwrap(), AuctionError::InvalidAuctionTimeError);
+
+        let state = &mut ctx.accounts.state;
+
+        state.auction_stage = AuctionStage::Open.to_u8();
+        state.seller = *ctx.accounts.initializer.key;
+        state.auction_duration = auction_duration;
+
+        let treasury = &mut ctx.accounts.treasury;
+        treasury.bump = *ctx.bumps.get("treasury").unwrap();
         Ok(())
     }
     /// Bid
-    pub fn bid(ctx: Context<Initialize>) -> Result<()> {
-        // ...
+    pub fn bid(ctx: Context<Bid>, lamports: u64) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        let treasury  = &mut ctx.accounts.treasury;
+        let bidder = &mut ctx.accounts.bidder;
+        let user = &mut ctx.accounts.user;
+        let system_program = &mut ctx.accounts.system_program;
+
+        require!(now_ts().unwrap() < state.auction_duration, AuctionError::InvalidAuctionTimeError);
+        require!(lamports >= sol_to_lamports(0.01), AuctionError::InvalidBidError);
+
+        invoke(
+            &system_instruction::transfer(
+                &user.to_account_info().key(),
+                &treasury.to_account_info().key(),
+                lamports
+            ),
+            &[
+                user.to_account_info().clone(),
+                treasury.to_account_info().clone(),
+                system_program.to_account_info().clone()
+            ],
+        )?;
+
+        bidder.bump = *ctx.bumps.get("bidder").unwrap();
+        bidder.bidder = *ctx.accounts.user.key;
+        bidder.bid = lamports;
+
+        if state.highest_bid < bidder.bid {
+            state.highest_bid = bidder.bid;
+            state.highest_bidder = bidder.bidder;
+        }
+
         Ok(())
     }
     /// After an auction ends (determined by `auction_duration`), a seller can claim the
     /// heighest bid by calling this instruction
-    pub fn end_auction(ctx: Context<Initialize>) -> Result<()> {
-        // ...
+    pub fn end_auction(ctx: Context<EndAuction>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        let treasury  = &mut ctx.accounts.treasury;
+        let initializer = &mut ctx.accounts.initializer;
+
+        require!(state.seller == initializer.key(), AuctionError::InvalidSigner);
+        require!(now_ts().unwrap() > state.auction_duration, AuctionError::InvalidAuctionTimeError);
+        require!(state.auction_stage == AuctionStage::Open.to_u8(), AuctionError::InvalidBidError);
+        require!(state.highest_bid > 0, AuctionError::InvalidBidError);
+        require!(**treasury.to_account_info().lamports.borrow() >= state.highest_bid, AuctionError::InvalidBalanceError);
+
+        **treasury.to_account_info().try_borrow_mut_lamports()? -= state.highest_bid;
+        **initializer.to_account_info().try_borrow_mut_lamports()? += state.highest_bid;
+
+        state.auction_stage = AuctionStage::Closed.to_u8();
+
         Ok(())
     }
     /// After an auction ends (the initializer/seller already received the winning bid), 
     /// the unsuccessfull bidders can claim their money back by calling this instruction
-    pub fn refund(ctx: Context<Initialize>) -> Result<()> {
-        // ...
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        let treasury  = &mut ctx.accounts.treasury;
+        let bidder = &mut ctx.accounts.bidder;
+        let user = &mut ctx.accounts.user;
+
+        require!(now_ts().unwrap() > state.auction_duration, AuctionError::InvalidAuctionTimeError);
+        require!(state.auction_stage == AuctionStage::Closed.to_u8(), AuctionError::InvalidBidError);
+        require!(bidder.bidder == user.key(), AuctionError::InvalidSigner);
+        require!(bidder.bid > 0, AuctionError::InvalidBidError);
+        require!(user.key() != state.highest_bidder, AuctionError::InvalidSigner);
+
+        **treasury.to_account_info().try_borrow_mut_lamports()? -= bidder.bid;
+        **user.to_account_info().try_borrow_mut_lamports()? += bidder.bid;
+
+        bidder.bid = 0;
+
         Ok(())
     }
 }
 
+fn now_ts() -> Result<u64> {
+    Ok(clock::Clock::get()?.unix_timestamp.try_into().unwrap())
+}
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    /// State of our auction program (up to you)
-    // #[account(...)]
-    pub state: Account<'info, State>,
-    /// Account which holds tokens bidded by biders
-    // #[account(...)]
-    pub treasury: AccountInfo<'info>,
-    /// Seller
-    // #[account(...)]
+    #[account(
+        init,
+        payer=initializer,
+        space=32+32+64+64+8+8
+    )]
+    pub state: Account<'info, AuctionState>,
+
+    #[account(
+        init,
+        payer=initializer,
+        space=8+8,
+        seeds=[b"treasury".as_ref()],
+        bump
+    )]
+    pub treasury: Account<'info, Treasury>,
+
+    #[account(mut)]
     pub initializer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Bid<'info> {
+    #[account(mut)]
+    pub state: Account<'info, AuctionState>,
+
+    #[account(
+        mut,
+        seeds=[b"treasury".as_ref()],
+        bump
+    )]
+    pub treasury: Account<'info, Treasury>,
+
+    #[account(
+        init,
+        payer=user,
+        space=8+32+64+8,
+        seeds=[b"bidder".as_ref(), user.key.as_ref()],
+        bump
+    )]
+    pub bidder: Account<'info, Bidder>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct EndAuction<'info> {
+    #[account(mut)]
+    pub state: Account<'info, AuctionState>,
+
+    #[account(
+        mut,
+        seeds=[b"treasury".as_ref()],
+        bump
+    )]
+    pub treasury: Account<'info, Treasury>,
+
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Refund<'info> {
+    #[account(mut)]
+    pub state: Account<'info, AuctionState>,
+
+    #[account(
+        mut,
+        seeds=[b"treasury".as_ref()],
+        bump
+    )]
+    pub treasury: Account<'info, Treasury>,
+
+    #[account(
+        mut,
+        seeds=[b"bidder".as_ref(), user.key.as_ref()],
+        bump
+    )]
+    pub bidder: Account<'info, Bidder>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
     pub system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct State {
-    // ...
+pub struct AuctionState {
+    pub seller: Pubkey, // 32 bytes
+    pub auction_duration: u64, // 64 bytes
+    pub highest_bidder: Pubkey, // 32 bytes
+    pub highest_bid: u64, // 64 bytes
+    pub auction_stage: u8, // 8 bytes
+}
+
+#[account]
+pub struct Treasury {
+    pub bump: u8, // 8 bytes
+}
+
+#[account]
+pub struct Bidder {
+    pub bump: u8, // 8 bytes
+    pub bidder: Pubkey, // 32 bytes
+    pub bid: u64 // 64 bytes
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum AuctionStage {
+    Open = 0,
+    Closed = 1,
+}
+
+impl AuctionStage {
+    fn to_u8(&self) -> u8 {
+        *self as u8
+    }
+
+    fn from_u8(stage: u8) -> Option<AuctionStage> {
+        match stage {
+            0 => Some(AuctionStage::Open),
+            1 => Some(AuctionStage::Closed),
+            _ => None
+        }
+    }
+}
+
+#[error_code]
+pub enum AuctionError {
+    #[msg("Invalid bid")]
+    InvalidBidError,
+    #[msg("Invalid time")]
+    InvalidAuctionTimeError,
+    #[msg("Invalid balance")]
+    InvalidBalanceError,
+    #[msg("Invalid auction state")]
+    InvalidAuctionState,
+    #[msg("Invalid signer")]
+    InvalidSigner
 }
--- tests/auction.ts
+++ tests/auction.ts
@@ -1,16 +1,272 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Auction } from "../target/types/auction";
+import { LAMPORTS_PER_SOL, PublicKey, SystemProgram } from "@solana/web3.js";
+import {expect, use} from "chai";
 
 describe("auction", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.Auction as Program<Auction>;
+    const program = anchor.workspace.Auction as Program<Auction>;
+    const state: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    const seller = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
+    const bidders = [
+        {bidder: anchor.web3.Keypair.generate(), bid: 0.1}, // refund bidder
+        {bidder: anchor.web3.Keypair.generate(), bid: 0.5}, // highest bidder
+        {bidder: anchor.web3.Keypair.generate(), bid: 0.2}, // refund bidder
+        {bidder: anchor.web3.Keypair.generate(), bid: 0.005}, // invalid bidder
+    ];
+
+    before(async function () {
+        await (program.provider as anchor.Provider).connection.requestAirdrop(seller.publicKey, LAMPORTS_PER_SOL)
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        for (const b of bidders) {
+            await (program.provider as anchor.Provider).connection.requestAirdrop(b.bidder.publicKey, LAMPORTS_PER_SOL)
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    });
+
+    it("is initialized!", async () => {
+        const [treasuryPDA, bump] = await PublicKey
+            .findProgramAddress(
+                [anchor.utils.bytes.utf8.encode("treasury")],
+                program.programId
+            );
+
+        const timestamp = new anchor.BN(Math.floor(Date.now() / 1000) + 5); // 15 seconds from now;
+
+        await program.methods
+            .initialize(timestamp)
+            .accounts({
+                state: state.publicKey,
+                initializer: seller.publicKey,
+                treasury: treasuryPDA,
+                systemProgram: SystemProgram.programId,
+            })
+            .signers([state, seller])
+            .rpc();
+
+        let treasuryState = await program.account.treasury.fetch(treasuryPDA);
+        let auctionState = await program.account.auctionState.fetch(state.publicKey);
+
+        expect(treasuryState.bump).to.equal(bump);
+        expect(auctionState.auctionDuration.toString()).to.equal(timestamp.toString());
+        expect(auctionState.auctionStage).to.equal(0)
+    });
+
+    it("has valid bids", async () => {
+        const [treasuryPDA, treasuryBump] = await PublicKey
+            .findProgramAddress(
+                [anchor.utils.bytes.utf8.encode("treasury")],
+                program.programId
+            );
+
+        const validBidders = [bidders[0], bidders[1], bidders[2]];
+
+        for (const bidder of validBidders) {
+            const [bidderPDA, bidderBump] = await PublicKey
+                .findProgramAddress(
+                    [
+                        anchor.utils.bytes.utf8.encode("bidder"),
+                        bidder.bidder.publicKey.toBytes()
+                    ],
+                    program.programId
+                );
+
+            const bid = new anchor.BN(LAMPORTS_PER_SOL * bidder.bid)
+
+            await program.methods
+                .bid(bid)
+                .accounts({
+                    state: state.publicKey,
+                    user: bidder.bidder.publicKey,
+                    treasury: treasuryPDA,
+                    bidder: bidderPDA,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([bidder.bidder])
+                .rpc();
+
+            let bidderState = await program.account.bidder.fetch(bidderPDA);
+            let bidderStateBid = new anchor.BN(bidderState.bid)
+
+            const balance = await (program.provider as anchor.Provider).connection.getBalance(treasuryPDA);
+
+            expect(bidderState.bump).to.equal(bidderBump);
+            expect(bidderState.bidder.toBase58()).to.equal(bidder.bidder.publicKey.toBase58())
+            expect(bidderStateBid.toString()).to.equal(bid.toString());
+        }
+    });
+
+    it("is invalid bid", async () => {
+        const [treasuryPDA, treasuryBump] = await PublicKey
+            .findProgramAddress(
+                [anchor.utils.bytes.utf8.encode("treasury")],
+                program.programId
+            );
+
+        const invalidBidder = bidders[3];
+
+        const [bidderPDA, bidderBump] = await PublicKey
+            .findProgramAddress(
+                [
+                    anchor.utils.bytes.utf8.encode("bidder"),
+                    invalidBidder.bidder.publicKey.toBytes()
+                ],
+                program.programId
+            );
+
+        const bid = new anchor.BN(LAMPORTS_PER_SOL * invalidBidder.bid)
+
+        let error = null;
+
+        try {
+            await program.methods
+                .bid(bid)
+                .accounts({
+                    state: state.publicKey,
+                    user: invalidBidder.bidder.publicKey,
+                    treasury: treasuryPDA,
+                    bidder: bidderPDA,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([invalidBidder.bidder])
+                .rpc()
+        } catch (e) {
+            error = e.message
+        }
+
+        expect(error).to.equal("6000: Invalid bid")
+    });
+
+    it("is invalid time to bid", async () => {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        const [treasuryPDA, treasuryBump] = await PublicKey
+            .findProgramAddress(
+                [anchor.utils.bytes.utf8.encode("treasury")],
+                program.programId
+            );
+
+        const invalidBidder = bidders[3];
+
+        const [bidderPDA, bidderBump] = await PublicKey
+            .findProgramAddress(
+                [
+                    anchor.utils.bytes.utf8.encode("bidder"),
+                    invalidBidder.bidder.publicKey.toBytes()
+                ],
+                program.programId
+            );
+
+        const bid = new anchor.BN(LAMPORTS_PER_SOL * invalidBidder.bid)
+
+        let error = null;
+
+        try {
+            await program.methods
+                .bid(bid)
+                .accounts({
+                    state: state.publicKey,
+                    user: invalidBidder.bidder.publicKey,
+                    treasury: treasuryPDA,
+                    bidder: bidderPDA,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([invalidBidder.bidder])
+                .rpc()
+        } catch (e) {
+            error = e.message
+        }
+
+        expect(error).to.equal("6001: Invalid time")
+    });
+
+    it("has highest bid", async () => {
+        const highestBidder = bidders[1];
+
+        const [bidderPDA, bidderBump] = await PublicKey
+            .findProgramAddress(
+                [
+                    anchor.utils.bytes.utf8.encode("bidder"),
+                    highestBidder.bidder.publicKey.toBytes()
+                ],
+                program.programId
+            );
+
+        let bidderState = await program.account.bidder.fetch(bidderPDA);
+        let bidderStateBid = new anchor.BN(bidderState.bid)
+
+        expect(bidderStateBid.toString()).to.equal(new anchor.BN(highestBidder.bid * LAMPORTS_PER_SOL).toString())
+        expect(bidderState.bidder.toBase58()).to.equal(highestBidder.bidder.publicKey.toBase58())
+    });
+
+    it("did pay auctioneer", async () => {
+        const initBalance = await (program.provider as anchor.Provider).connection.getBalance(seller.publicKey);
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        const [treasuryPDA, bump] = await PublicKey
+            .findProgramAddress(
+                [anchor.utils.bytes.utf8.encode("treasury")],
+                program.programId
+            );
+
+        await program.methods
+            .endAuction()
+            .accounts({
+                state: state.publicKey,
+                initializer: seller.publicKey,
+                treasury: treasuryPDA,
+                systemProgram: SystemProgram.programId,
+            })
+            .signers([seller])
+            .rpc();
+
+        let auctionState = await program.account.auctionState.fetch(state.publicKey);
+        const balance = await (program.provider as anchor.Provider).connection.getBalance(seller.publicKey);
+
+        expect(balance).to.greaterThan(initBalance);
+        expect(auctionState.auctionStage).to.equal(1)
+    });
+
+    it("did refund bidders", async () => {
+        const [treasuryPDA, treasuryBump] = await PublicKey
+            .findProgramAddress(
+                [anchor.utils.bytes.utf8.encode("treasury")],
+                program.programId
+            );
+
+        const validBidders = [bidders[0], bidders[2]];
+
+        for (const bidder of validBidders) {
+            const initBalance = await (program.provider as anchor.Provider).connection.getBalance(bidder.bidder.publicKey);
+
+            const [bidderPDA, bidderBump] = await PublicKey
+                .findProgramAddress(
+                    [
+                        anchor.utils.bytes.utf8.encode("bidder"),
+                        bidder.bidder.publicKey.toBytes()
+                    ],
+                    program.programId
+                );
+
+            await program.methods
+                .refund()
+                .accounts({
+                    state: state.publicKey,
+                    user: bidder.bidder.publicKey,
+                    treasury: treasuryPDA,
+                    bidder: bidderPDA,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([bidder.bidder])
+                .rpc();
+
+            const balance = await (program.provider as anchor.Provider).connection.getBalance(seller.publicKey);
+            expect(balance).to.greaterThan(initBalance);
+        }
+    });
 });
